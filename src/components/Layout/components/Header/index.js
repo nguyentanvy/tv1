e76a114e54cd25431fb5fab8521fa9c28fa85906
nguyentanvy <1951120161@sv.ut.edu.vn>
2023-04-19 17:05:51 +0700

    make frame layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,12 @@
-function Header() {
-    return (<h2>Header</h2>  );
+import React from 'react'
+import styles from "./Header.module.scss"
+import classNames from 'classnames/bind'
+const cx = classNames.bind(styles)
+ const Header = () => {
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}></div>
+    </header>
+  )
 }
-
 export default Header;
